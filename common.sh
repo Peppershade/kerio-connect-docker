d8df742a0b3cd3042885a3197c51ffc2254b2297
Pepper <mc.peppertown@gmail.com>
2022-05-13 16:07:23 +0200

    Update common.sh

--- common.sh
+++ common.sh
@@ -6,8 +6,8 @@ CONNECT_BUILD=$(grep 'ENV CONNECT_BUILD' Dockerfile | awk {'print $3'})
 CONNECT_HOME=$(grep 'ENV CONNECT_HOME' Dockerfile | awk {'print $3'})
 
 THIS_HOME=$(pwd)
-IMAGE=${CONNECT_NAME}:${CONNECT_VERSION}.${CONNECT_BUILD}
-IMAGE_2=${CONNECT_NAME}:latest
+IMAGE=pernodpeper/${CONNECT_NAME}:${CONNECT_VERSION}.${CONNECT_BUILD}
+IMAGE_2=pernodpepper/${CONNECT_NAME}:latest
 
 function printBanner {
 	echo "**************************"
