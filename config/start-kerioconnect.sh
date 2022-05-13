a2317e277878ada68d3539c9889d0e7c4f37105c
Pepper <mc.peppertown@gmail.com>
2022-05-13 16:03:31 +0200

    Update start-kerioconnect.sh

--- config/start-kerioconnect.sh
+++ config/start-kerioconnect.sh
@@ -2,11 +2,11 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    echo "* Starting Kerio Connect"
+    echo " * Starting Kerio Connect"
     /usr/bin/supervisord > /dev/null 2>&1 &
     sleep 15
     service kerio-connect stop
-    echo "* Fixing permissions"
+    echo " * Fixing permissions"
     chmod 777 /opt/kerio/mailserver/mailserver.cfg
     chmod 777 /opt/kerio/mailserver/users.cfg
     chmod -R 777 /opt/kerio/mailserver
